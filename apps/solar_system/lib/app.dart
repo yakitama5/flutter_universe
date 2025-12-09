7882c75230742601c497ecba1494293c72699708
Yakuran <rittitarou@yahoo.co.jp>
2025-12-09 14:51:29 +0900

    ♻️refactor: 不要となった画面や機能を削除

--- apps/solar_system/lib/app.dart
+++ apps/solar_system/lib/app.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'screens/model_check.dart';
-import 'screens/random_universe.dart';
-import 'screens/universe.dart';
-
-enum UniverseType { universe, randomUniverse, modelCheck }
+import 'package:solar_system/screens/random_universe.dart';
 
 /// アプリケーションのメインウィジェット
 class App extends StatefulWidget {
@@ -19,7 +15,6 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   late Ticker ticker;
   double elapsedSeconds = 0;
-  UniverseType _selectedUniverseType = UniverseType.universe;
 
   @override
   void initState() {
@@ -33,70 +28,14 @@ class _AppState extends State<App> {
     super.initState();
   }
 
-  Widget _buildUniverse() {
-    switch (_selectedUniverseType) {
-      case UniverseType.universe:
-        return Universe(elapsedSeconds: elapsedSeconds);
-      case UniverseType.randomUniverse:
-        return RandomUniverse(elapsedSeconds: elapsedSeconds);
-      case UniverseType.modelCheck:
-        return const ModelCheck();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Planetarium',
+      title: 'Solar System',
       home: Scaffold(
         backgroundColor: Colors.black,
-        appBar: AppBar(title: Text(_selectedUniverseType.name)),
-        body: Stack(
-          children: [
-            _buildUniverse(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: DropdownButton<UniverseType>(
-                    value: _selectedUniverseType,
-                    dropdownColor: Colors.black,
-                    style: const TextStyle(color: Colors.white),
-                    iconEnabledColor: Colors.white,
-                    underline: const SizedBox.shrink(),
-                    onChanged: (UniverseType? newValue) {
-                      if (newValue != null) {
-                        setState(() {
-                          _selectedUniverseType = newValue;
-                        });
-                      }
-                    },
-                    items: const [
-                      DropdownMenuItem(
-                        value: UniverseType.universe,
-                        child: Text('Universe'),
-                      ),
-                      DropdownMenuItem(
-                        value: UniverseType.randomUniverse,
-                        child: Text('Random Universe'),
-                      ),
-                      DropdownMenuItem(
-                        value: UniverseType.modelCheck,
-                        child: Text('Model Check'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+        appBar: AppBar(title: Text('Solar System')),
+        body: RandomUniverse(elapsedSeconds: elapsedSeconds),
       ),
     );
   }
