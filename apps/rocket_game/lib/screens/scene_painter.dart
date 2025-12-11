63360609b2ee0026701399b9183b97056c8c8a2a
yakitama5 <rittitarou@yahoo.co.jp>
2025-12-11 18:47:18 +0900

    🚧wip: キャラを操作して移動するアクションを追加

--- apps/rocket_game/lib/screens/scene_painter.dart
+++ apps/rocket_game/lib/screens/scene_painter.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scene/scene.dart';
-import 'package:vector_math/vector_math.dart';
 
 class RocketScenePainter extends CustomPainter {
   RocketScenePainter({required this.scene, required this.camera});
@@ -10,12 +9,6 @@ class RocketScenePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO(yakitama5): テスト用
-    final camera = PerspectiveCamera(
-      position: Vector3(0, 3, 0),
-      target: Vector3(0, 0, 30),
-    );
-
     scene.render(camera, canvas, viewport: Offset.zero & size);
   }
 
